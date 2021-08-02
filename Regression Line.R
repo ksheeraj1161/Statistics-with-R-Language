ea400ff364e92525714cbfa975c697f18dd08a2e
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-08-02 17:44:45 +0530

    Create Regression Line.R

---
+++ Regression Line.R
@@ -0,0 +1,11 @@
+# Equation of regression line of X on Y
+
+> X=c(4.7,8.2,12.4,15.8,20.7,24.9,31.9,35.0,39.1,38.8)
+> Y=c(4.0,8.0,12.5,16.0,20.0,25.0,31.0,36.0,40.0,40.0)
+> Reg=lm(X~Y)
+> Reg
+Call:
+lm(formula = X ~ Y)
+Coefficients:
+(Intercept) Y 
+ 0.7508 0.9634
