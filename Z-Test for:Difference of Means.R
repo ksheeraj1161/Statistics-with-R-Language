a45c4a4cfd12adf096959dea0ed4ff40bd6dde3a
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-08-03 17:24:43 +0530

    Create Z-Test for:Difference of Means.R

---
+++ Z-Test for:Difference of Means.R
@@ -0,0 +1,26 @@
+> print("H0 := x1=x2")
+[1] "H0 := x1=x2"
+> print("H1 := x1!=x2")
+[1] "H1 := x1!=x2"
+> alpha=0.05
+> ZTab=qnorm(1-alpha)
+> ZTab
+[1] 1.644854
+> n1=32
+> n2=36
+> x1=72
+> x2=70
+> s1=8
+> print("H0 := x1=x2")
+[1] "H0 := x1=x2"
+> print("H1 := x1!=x2")
+[1] "H1 := x1!=x2"
+> alpha=0.05
+> ZTab=qnorm(1-alpha)
+> ZTab
+[1] 1.644854
+> n1=32
+> n2=36
+> x1=72
+> x2=70
+> s1=8
